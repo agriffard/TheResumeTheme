dd75d1ef031b4d6a977c84cab21986ee31efef93
Ferenc Győri <ferenc.gyori@lombiq.com>
2021-09-27 20:07:20 +0200

    Updating to 1.0.0

---
+++ src/TheResumeTheme/ResourceManagementOptionsConfiguration.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Options;
+using OrchardCore.ResourceManagement;
+
+namespace TheResumeTheme
+{
+
+
+    public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
+    {
+        private static readonly ResourceManifest _manifest = new ResourceManifest();
+
+        static ResourceManagementOptionsConfiguration()
+        {
+            _manifest
+               .DefineScript("Resume")
+               .SetDependencies("bootstrap") //, jQuery.easing
+               .SetUrl("~/TheResumeTheme/js/scripts.min.js", "~/TheResumeTheme/js/scripts.js")
+               .SetVersion("6.0.0");
+
+            _manifest
+                .DefineStyle("Resume")
+                .SetUrl("~/TheResumeTheme/css/styles.min.css", "~/TheResumeTheme/css/styles.css")
+                .SetVersion("6.0.0");
+
+        }
+
+        public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
+    }
+}
--- src/TheResumeTheme/Startup.cs
+++ src/TheResumeTheme/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OrchardCore.Modules;
 using OrchardCore.ResourceManagement;
 
@@ -6,9 +7,7 @@ namespace TheResumeTheme
 {
     public class Startup : StartupBase
     {
-        public override void ConfigureServices(IServiceCollection serviceCollection)
-        {
-            serviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
-        }
+        public override void ConfigureServices(IServiceCollection services) =>
+               services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
     }
 }
